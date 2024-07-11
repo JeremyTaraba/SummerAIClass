d42c3f90d2d413841b3b98490d59e9ab271fa55f
Jeremy Taraba <jeremytaraba@gmail.com>
2024-07-11 09:50:01 -0700

    Update recommender_system.py

--- recommender_system.py
+++ recommender_system.py
@@ -1,8 +1,6 @@
 from sklearn import svm
 
-games_list = [
-    "Fortnite", "Among Us", "Zelda", "League", "Elden Ring"
-]
+
 
 # Graphics, Multiplayer, Age, Difficulty, Price
 games_score_list = [
@@ -13,8 +11,12 @@ games_score_list = [
     [9, 1, 1, 10, 6 ],
 ]
 
-my_ideal_game = [9,1,1,9,0]
-weights_list = [100,1,50,100,1]
+games_list = [
+    "Fortnite", "Among Us", "Zelda", "League", "Elden Ring"
+]
+
+my_ideal_game = [4,1,4,3,0]
+# weights_list = [10,1,5,10,1]
 recommendation_model = svm.SVC()
-recommendation_model.fit(games_score_list, games_list,weights_list)
+recommendation_model.fit(games_score_list, games_list)
 print(recommendation_model.predict([ my_ideal_game]))   
